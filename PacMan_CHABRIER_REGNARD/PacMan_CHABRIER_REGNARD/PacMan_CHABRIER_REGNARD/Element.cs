1543289509f209daad9e582f2337bc1d0d4d07d7
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-08 19:34:09 +0100

    corrected

---
+++ PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/Element.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacMan_CHABRIER_REGNARD
+{
+    public enum Element
+    {
+        Wall, Beans, BigBeans, Nothing
+    }
+}
