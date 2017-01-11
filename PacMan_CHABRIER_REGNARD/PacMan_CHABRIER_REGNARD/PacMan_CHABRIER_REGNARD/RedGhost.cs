ee1ac284751a2798d4736aabf32a655cc90f6d32
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-01-11 19:05:21 +0100

    Adding red ghost AI

---
+++ PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/RedGhost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacMan_CHABRIER_REGNARD
+{
+    class RedGhost : Ghost
+    {
+        protected override void computeTargetTile(PacMan pac)
+        {
+            this.target = new Position(pac.getPosition().getPosX(), pac.getPosition().getPosY());
+        }
+    }
+}
