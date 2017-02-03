54579c22d8097726f769d6bfa3f8a52d23060e1b
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-02-03 10:38:20 +0100

    Creater Clyde's AI and his way of computing target

---
+++ PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/YellowGhost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacMan_CHABRIER_REGNARD
+{
+    class YellowGhost : Ghost
+    {
+        protected override void computeTargetTile(PacMan pac)
+        {
+            switch (this.mode)
+            {
+                case Mode.Scatter:
+                    target = new Position(32, 0);
+                    break;
+                case Mode.Normal:
+                    int distance = manhattanDistance(pac.getPosition(), this.getPosition());
+                    if (distance < 8)
+                    {
+                        target = new Position(32, 0);
+                    }
+                    else
+                    {
+                        target = new Position(pac.getPosition().getPosX(), pac.getPosition().getPosY());
+                    }
+                    break;
+            }
+
+            
+        }
+    }
+}
