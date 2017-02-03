a35758ef21e0f751ed44b07a45fc8eb9ca17dc71
Mathieu Regnard <mathieu.regnard2@gmail.com>
2017-02-03 10:37:40 +0100

    Creater Pinky AI and his way of computing target

---
+++ PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/PacMan_CHABRIER_REGNARD/PinkGhost.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PacMan_CHABRIER_REGNARD
+{
+    class PinkGhost : Ghost
+    {
+        protected override void computeTargetTile(PacMan pac)
+        {
+
+            switch (this.mode)
+            {
+                case Mode.Scatter:
+                    target = new Position(-1, 2);
+                    break;
+                case Mode.Normal:
+                    Position pos = fourAhead(pac.getState(), pac.getPosition());
+                    if (pac.getState() == State.Up)
+                    {
+                        pos.setPosY(pos.getPosY() - 4);
+                    }
+                    target = pos;
+                    break;
+            }
+           
+        }
+
+        private Position fourAhead(State d, Position p)
+        {
+            switch (d)
+            {
+                case State.Up:
+                    return new Position(p.getPosX() - 4, p.getPosY());
+                case State.Down:
+                    return new Position(p.getPosX() + 4, p.getPosY());
+                case State.Left:
+                    return new Position(p.getPosX(), p.getPosY() - 4);
+                case State.Right:
+                    return new Position(p.getPosX(), p.getPosY() + 4);
+                default:
+                    return p;
+
+            }
+        }
+    }
+}
+
